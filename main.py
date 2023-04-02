cd768994265e23da73f4d5bf550986cdc75f6f29
merricatbw <zkerr4032@outlook.com>
2023-04-02 20:23:17 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+def main():
+    registers = {
+        "R0": 0,
+        "R1": 0,
+        "R2": 0,
+        "R3": 0
+    }
+    
+    stack = []
+    
+    
+    
+def decode_register(encoded):
+    table = {
+        0: "R0",
+        1: "R1",
+        2: "R2",
+        3: "R3"
+    }
+    
+    return table[encoded]
+
+
+
+if __name__ == "__main__":
+    main()
